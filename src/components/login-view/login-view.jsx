91416125d86ab5000c036763b37ef171a0877f39
Hermann Rasch <hermann.rasch93@gmail.com>
2023-02-21 20:59:11 +0100

    Create login-view.jsx

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+// import { useState } from "react";
+
+const [username, setUsername] = useState("");
+export const LoginView = ({ onLoggedIn }) => {
+ const handleSubmit = (event) =>{ 
+    //prevents default behavior of the form which is to reload the entire page
+    event.preventDefault();
+    const data = {
+        access: username,
+        secret: password,
+        };
+    fetch("https://movies-couch-api.vercel.app/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)        
+        })
+     .then((response) => response.json())
+     .then((data)  => {
+        console.log("Login response: ", data);
+        if (data.user) {
+            localStorage.setItem("user", JSON.stringify(data.user));
+            localStorage.setItem("token", data.token);
+            onLoggedIn(data.user, data.token);
+        } else {
+            alert("No such user");
+        }
+    })
+    .catch((e) => {
+        alert("Something went wrong!");
+    });
+ };
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Username:
+                <input type= "text" 
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                minLength="5"/>
+            </label>
+            <label> 
+                Password:
+                <input type="password"
+                 value={password}
+                 onChange={(e) => setPassword(e.target.value)}
+                 required />
+            </label>
+        </form>
+    );
+}
