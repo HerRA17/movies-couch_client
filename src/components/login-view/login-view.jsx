ee8cbb8ed1a0bb527e380f254ed55de7acc976b4
Hermann Rasch <hermann.rasch93@gmail.com>
2023-02-22 20:34:03 +0100

    Update login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 export const LoginView = ({ onLoggedIn }) => {
  const [username, setUsername] = useState("");
  const [password, setPassword] = useState("");
+//  validation of user login
  const handleSubmit = (event) =>{ 
     //prevents default behavior of the form which is to reload the entire page
     event.preventDefault();
@@ -24,8 +25,8 @@ export const LoginView = ({ onLoggedIn }) => {
         console.log("Login response: ", data);
         if (data.user) {
             localStorage.setItem("user", JSON.stringify(data.user));
-            localStorage.setItem("token", data.token);
-            onLoggedIn(data.user, data.token);
+            // localStorage.setItem("token", data.token);
+            onLoggedIn(data.user /*, data.token*/);
         } else {
             alert("No such user");
         }
@@ -34,7 +35,9 @@ export const LoginView = ({ onLoggedIn }) => {
         alert("Something went wrong!");
     });
  };
+//  login form with submit button
     return (
+        // handle submit is the callback of onSubmit, tells the login API to validate user & password
         <form onSubmit={handleSubmit}>
             <label>
                 Username:
