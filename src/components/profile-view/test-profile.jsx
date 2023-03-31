0086c33569771590a01476fc973e00aa6a9040a5
Hermann Rasch <hermann.rasch93@gmail.com>
2023-03-31 14:01:57 +0200

    Revisions and corrections on the get, update & delete functions

--- src/components/profile-view/test-profile.jsx
+++ src/components/profile-view/test-profile.jsx
@@ -1,9 +1,13 @@
 import  {React, useEffect, useState} from "react"; 
-// import Form from "react-bootstrap/Form";
-import { Col, ListGroup, ListGroupItem, Row}  from "react-bootstrap";
+import Form from "react-bootstrap/Form";
+import { Col, ListGroup, Row}  from "react-bootstrap";
 import { Card, Container } from "react-bootstrap";
-import axios from "axios";
-import FavoriteMovies from "./favorite-movies";
+// import { toast } from "react-bootstrap"; //primary, success, danger, warning
+import Button from "react-bootstrap/Button";
+import PropTypes from "prop-types";
+// import FavoriteMovies from "./favorite-movies";
+import { PropTypes } from "react-bootstrap";
+import { toast } from "react-toastify";
 
 
     export const TestProfile= ({user, setUser, movies, token, onLoggedOut}) => {
@@ -12,22 +16,37 @@ import FavoriteMovies from "./favorite-movies";
         const [password, setPassword] = useState("");
         const [email, setEmail] = useState("");
         const [birthday, setBirthday] = useState("");
+
         //get users 
-    useEffect(() =>{
-        const token = window.localStorage.getItem("token");
-        axios.get(`https://movies-couch-api.vercel.app/users/${user.Username}`,
-        {headers: {Authorization: `Bearer ${token}`}})
-        .then(response => {
-            setUser(response.data);
+    useEffect(() => {
+        fetch(`https://movies-couch-api.vercel.app/users/${user.Username}`,
+        {
+            method: "GET",
+            headers: { Authorization: `Bearer ${token}` },
         })
-        .catch(error => {
+        // conditional and check status- response.ok else trhow error
+        .then((response) => {
+            // response.json())
+        console.log(response);
+        if (response.ok) {
+            toast.success("User data retrieved")
+        } else {
+            toast.danger("User data coudl not be retrieved.")
+        }
+            // .then((data) => {
+            // console.log(data);
+            //   setFavoriteMovies(data.favoriteMovies) 
+        })
+        .catch((error) => {
             console.error("An error ocurred" + error);
+            // alert("error");
+            // toast.danger("error");
         });
-    }, [user.Username, token]);
+    }, [user.username, token]);
     
         //    update user function
-        // implement function such as in userupdate file
-            const handleSubmit= (event) => {
+        const updateUser = function (){
+            function handleSubmit(event) {
                 event.preventDefault();
                 const token = localStorage.getItem("token");
                 const data = {
@@ -35,69 +54,83 @@ import FavoriteMovies from "./favorite-movies";
                     Password: password,
                     Email: email,
                     Birthday: birthday
-                }
-                axios.put(`https://movies-couch-api.vercel.app/users/${user.Username}`,
-                data,
-                {headers: {Authorization: `Bearer ${token}`}}
+                };
+                fetch(`https://movies-couch-api.vercel.app/users/${user.Username}`,
+                    data,
+                    {
+                        method: "PUT",
+                        headers: { Authorization: `Bearer ${token}` }
+                    }
                 )
-                .then((response) =>{
-                    console.log(response.data);
-                    alert("Profile updated!");
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-        };
-        const handleUpdate = (e) => {
-            e.preventDefault();
-            setUsername(e.target.value);
-            setPassword(e.target.value);
-            setEmail(e.target.value);
-            setBirthday(e.target.value);
-        }   
-    
+                    .then((response) => {
+                        console.log(response);
+                        if (response.ok) {
+                            alert("Profile updated!");
+                        } else {
+                            alert("Update Failed!");
+                        }
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+            }
+            const handleUpdate = (e) => {
+                e.preventDefault();
+                setUsername(e.target.value);
+                setPassword(e.target.value);
+                setEmail(e.target.value);
+                setBirthday(e.target.value);
+            }   
+        }
+        
     // delete user
-    const deleteUser = function () {
-        axios.delete(`https://movies-couch-api.vercel.app/users/${user.Username}`, 
-        {headers: {Authorization: `Bearer ${token}`}})
-        .then(function (response) {
-            if (response.status === 401) {
-                throw new Error("Sorry, you're not authorized to access this resource.");
-            } else if( response.status === 404) {
-                throw new Error("User was not found.")
-            } else if(response.ok) {
-                toast.success(`You succesfully deleted the account with the username ${user.Username}.`);
-                onLoggedOut();
-            }
-        })
-        .catch(function (error) {
-            if (error.message) {
-                toast.error(error.message);
-            } else {
-                toast.error("An error ocurred while trying to delete. Please try again later.");
-            }
-            console.error("An error occured: " + error)
-        });
-    };
+    // const deleteUser = function () {
+    //     fetch(`https://movies-couch-api.vercel.app/users/${user.Username}`, 
+    //     {
+    //         method: "DELETE",
+    //         headers: {Authorization: `Bearer ${token}`}})
+    //     .then(function (response) {
+    //         console.log(response);
+    //         if (response.status === 401) {
+    //             throw new Error("Sorry, you're not authorized to access this resource.");
+    //         } else if( response.status === 404) {
+    //             throw new Error("User was not found.")
+    //         } else if(response.ok) {
+    //             toast.success(`You succesfully deleted the account with the username ${user.Username}.`);
+    //             onLoggedOut();
+    //         }
+    //     })
+    //     .catch(function (error) {
+    //         if (error.message) {
+    //             toast.error(error.message);
+    //         } else {
+    //             toast.error("An error ocurred while trying to delete. Please try again later.");
+    //         }
+    //         console.error("An error occured: " + error)
+    //     });
+    // };
+    // let moviesId = movies_.id
     // Fav-movies 
-    const favoriteMovies = movies.filter(m => user.FavoriteMovies.includes(m._id)) 
+    // const favoriteMovies = movies.filter((movie) => favoriteMovies.includes(movie._id)) //change m._id
     // remove-fav_Movies
-  removeFavMovie = async (movies) =>{
-    const user = localStorage.getItem("user");
-    console.log(user);
-    const token = localStorage.getItem("token");
-    console.log(token);
-    axios.delete(`https://movies-couch-api.vercel.app/users/${user.Username}/movies/${movies._id}`,
-    {header: {Authorization: `Bearer ${token}`}}
-    ) 
-    .then((response) => {
-        console.log(response);
-        alert(`The Movie: ${movies._id} was removed from Favorite List`)
-    })
-    .catch(function (error) {
-        console.error(" An error ocurred" +error);
-    })
- }
+//   removeFavMovie = async (movies) =>{
+//     const user = localStorage.getItem("user");
+//     console.log(user);
+//     const token = localStorage.getItem("token");
+//     console.log(token);
+//     fetch(`https://movies-couch-api.vercel.app/users/${user.Username}/movies/${movies._id}`,
+//     {
+//       method: "DELETE",
+//      header: {Authorization: `Bearer ${token}`}}
+//     ) 
+//     .then((response) => {
+//         console.log(response);
+//         toast.success(`The Movie: ${movies._id} was removed from Favorite List`)
+//     })
+//     .catch(function (error) {
+//         console.error(" An error ocurred" +error);
+//     })
+//  }
 
 
     return (
@@ -105,6 +138,7 @@ import FavoriteMovies from "./favorite-movies";
                 <Row className="d-flex justify-content-center p-4">
                     <Col >
                         <Card>
+                        <Card.Title>Profile Information</Card.Title>
                             <Card.Body className="profile-view">
                             <>
                              <h3>Username:</h3>
@@ -127,71 +161,64 @@ import FavoriteMovies from "./favorite-movies";
                    <Row className="d-flex justify-content-center p-4">
                         <Col >
                             <Card>
+                                <Card.Title>Update Form</Card.Title>
                                 <Card.Body className="form-profile">
-                                    <Card.Title>Profile Information</Card.Title>
-                                    <Card.Text></Card.Text>
-                                    <ListGroup>
-                                        <form onSubmit={handleSubmit}>
-                                            <ListGroup.Item>
-                                            <label>
-                                                Username:
-                                                <input 
-                                                type="text"
-                                                name="username"
-                                                class=""
-                                                // value={user.Username}
-                                                onChange={(e) => handleUpdate(e)}
-                                                /> 
-                                            </label>
-                                            </ListGroup.Item>
+                                    <Form /*onSubmit={handleSubmit}*/>
+                                            <Form.Group>
+                                            <Form.Label aria-label="username"> Username:</Form.Label>
+                                            <Form.Control 
+                                            type="text"
+                                            name="username"
+                                            className=""
+                                            value={user.Username}
+                                            onChange={(e) => handleUpdate(e)}
+                                            // aria-required="true"
+                                            />
+                                            </Form.Group>
                                             <br/>
-                                           <ListGroup.Item>   
-                                            <label>
-                                                Password:
-                                                <input 
+                                           <Form.Group >   
+                                            <Form.Label aria-label="password"> Password:</Form.Label>
+                                                
+                                                <Form.Control
                                                 type="password"
                                                 name="password"
-                                                class=""
+                                                className=""
                                                 // value={user.Password}
                                                 onChange={(e) => handleUpdate(e)}
                                                  />
-                                            </label>
-                                            </ListGroup.Item> 
+                                            
+                                            </Form.Group> 
                                             <br />
-                                            <ListGroup.Item>  
-                                            <label>
-                                                Email:
-                                                <input 
+                                            <Form.Group>  
+                                            <Form.Label aria-label="email">Email:</Form.Label>
+                                                <Form.Group 
                                                 type="email"
                                                 name="email"
-                                                class=""
-                                                // value={user.Email}
+                                                className=""
+                                                value={user.Email}
                                                 onChange={(e) => handleUpdate(e)}  
                                                 />
-                                            </label>
-                                            </ListGroup.Item>   
+                                            
+                                            </Form.Group>   
                                             <br />
-                                            <ListGroup.Item>
-                                            <label>
-                                                Birthday: 
-                                                <input 
+                                            <Form.Group>
+                                            <Form.Label aria-label="birthday"> Birthday:</Form.Label> 
+                                                <Form.Control 
                                                 type="date" 
                                                 name="birthday"
-                                                class=""
-                                                // value={user.Birthday}
+                                                className=""
+                                                value={user.Birthday}
                                                 onChange={(e) => handleUpdate(e)}
                                                  />
-                                            </label>
-                                            </ListGroup.Item>
+                                            </Form.Group>
                                             <br />
                                             <ListGroup.Item>
-                                            <button  type="submit" class="" onChange={()=> updateUser()}>Update User</button>
+                                            <Button  type="submit" className="updateButton" /*onChange={()=> updateUser()}*/>Update User</Button>
                                             </ListGroup.Item>
                                             <ListGroup.Item>
-                                            <button type="submit" class="" onChange={()=> deleteUser()} >Delete User</button>
+                                            <Button type="submit" className="deleteButton" /*onChange={()=> deleteUser()} */ >Delete User</Button>
                                             </ListGroup.Item>
-                                        </form>
-                                    </ListGroup>
+                                    </Form>
                                 </Card.Body>
                             </Card>
                         </Col>
@@ -205,12 +232,12 @@ import FavoriteMovies from "./favorite-movies";
             </Container>
         ); 
 };
+
+export default TestProfile;
 // TestProfile.propTypes = {
-//     user: Proptypes.shape({
-//     _id: Proptypes.number.isRequired,
-//     username: Proptypes.string.isRequired,
-//     password: Proptypes.string.isRequired,
-//     email: Proptypes.string.isRequired,
-//     birthday: Proptypes.date.isRequired
-// })
-// };
+//     user: PropTypes.object,
+//     // setUser: PropTypes.instanceOf(user), 
+//     movies: PropTypes.object, 
+//     token: PropTypes.number,
+//     onLoggedOut: PropTypes.func
+// }
