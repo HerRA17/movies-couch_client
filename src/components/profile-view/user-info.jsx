150dcac9872c78883b817f7567beb3a34c696a72
Hermann Rasch <hermann.rasch93@gmail.com>
2023-04-12 13:41:58 +0200

    Change to the structure

--- src/components/profile-view/user-info.jsx
+++ src/components/profile-view/user-info.jsx
@@ -1,15 +1,31 @@
 import React from "react";
+import { Button, Card, Container } from "react-bootstrap";
 
-function UserInfo({ Username, Email, Birthday}) {
+function UserInfo({ user }) {
+    console.log(UserInfo);
     return(
         <>
-        <h4>Your info</h4>
-        <p placholder="Name" >{user.Username}</p>
-        <br/>
-        <p placholder="e-mail">{user.Email}</p>
-        <br/>
-        <p placholder="birthday">{user.Birthday}</p>
+            <h2>Your Profile:</h2>
+                <Card>
+                    <Card.Body className="profile-view">
+                        <>
+                            <h3>Username:</h3>
+                            <p>{user.Username}</p>
+                            <br />
+                            {/* <h3>Password:</h3> 
+                            <p>{user.Password}</p> */}
+                            <br />
+                            <h3>Email:</h3>
+                            <p>{user.Email}</p>
+                            <br />
+                            <h3>Birthday:</h3>
+                            <p>{user.Birthday}</p>
+                            <br />
+                        </>
+                    </Card.Body>
+                </Card>
         </>
     )
 }
-export {UserInfo}; 
+
+export { UserInfo }; 
