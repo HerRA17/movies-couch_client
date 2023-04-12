2db9bf64ef4ad3c9b4317b39a2cefe060d5a2db4
Hermann Rasch <hermann.rasch93@gmail.com>
2023-04-12 13:41:09 +0200

    minor adjustments

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -1,29 +1,29 @@
 import { useState } from "react";
-import Form from"react-bootstrap/Form";
+import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
-import Row  from "react-bootstrap/Row";
+import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button"; 
 
 export const SignupView = () => {
-    const [username, setUsername] = useState(""); 
+    const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [email, setEmail] = useState("");
     const [birthday, setBirthday] = useState("");
 
-// validation of signup view
+    // validation of signup view
     const handleSubmit = (event) => {
         event.preventDefault();
     const data = {
-        Username: username,
-        Password: password,
-        Email: email,
-        Birthday: birthday
+            Username: username,
+            Password: password,
+            Email: email,
+            Birthday: birthday
         };
 
     fetch("https://movies-couch-api.vercel.app/users", {
-        method: "POST",
+         method: "POST",
          body: JSON.stringify(data),         
          headers: {
             "Content-Type": "application/json"
@@ -37,57 +37,59 @@ export const SignupView = () => {
                 } 
             });
          };
-// signup form with submit button
+    // signup form with submit button
         return (
-// handleSubmit is the callback of onSubmit, tells the login API to validate user & password
-<Container>
-    <Row>
-        <p class="movies-couch">Logo</p>
-    </Row>
-    <Row>
-        <Col>
-            <Card>
-                <Card.Body className="movies-couch">
-                    <Form className="movies-couch" onSubmit={handleSubmit}>
-                                <Form.Group controlId="formUsername">
-                                    <Form.Label>Username:</Form.Label>
-                                <Form.Control type="text" 
-                                value={username}
-                                onChange={(e) => setUsername(e.target.value)}
-                                required
-                                minLength="3"
-                                />
-                        </Form.Group>
-                        <Form.Group controlId="formPassword">
-                            <Form.Label>Password:</Form.Label>
-                            <Form.Control type="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required 
-                            minLength="5" />
-                        </Form.Group>
-                        <Form.Group controlId="formEmail">
-                            <Form.Label>Email:</Form.Label>
-                            <Form.Control type="text" 
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            required/>
-                        </Form.Group>
-                        <Form.Group controlId="Birthday">
-                            <Form.Label>Birthday:</Form.Label>
-                            <Form.Control type="date"
-                            value={birthday}
-                            onChange={(e) => setBirthday(e.target.value)}
-                            required
-                            />
-                        </Form.Group>
-                                <Button variant="primary" type="submit">Submit</Button> 
-                    </Form> 
+    // handleSubmit is the callback of onSubmit, tells the login API to validate user & password
+    <Container>
+        <Row>
+            <p className="movies-couch">Logo</p>
+        </Row>
+        <Row>
+            <Col>
+                <Card>
+                    <Card.Body className="movies-couch">
+                        <Form className="movies-couch" onSubmit={handleSubmit} >
+                                    <Form.Group controlId="formUsername">
+                                        <Form.Label aria-label="username">Username:</Form.Label>
+                                    <Form.Control type="text" 
+                                        value={username}
+                                        onChange={(e) => setUsername(e.target.value)}
+                                        aria-required="true"
+                                        minLength="3"
+                                         />
+                                    </Form.Group>
+                                    <Form.Group controlId="formPassword">
+                                        <Form.Label aria-label="password">Password:</Form.Label>
+                                        <Form.Control type="password"
+                                            value={password}
+                                            onChange={(e) => setPassword(e.target.value)}
+                                            aria-required="true" 
+                                            minLength="5"
+                                             />
+                                    </Form.Group>
+                                    <Form.Group controlId="formEmail">
+                                        <Form.Label aria-label="email">Email:</Form.Label>
+                                        <Form.Control type="text" 
+                                            value={email}
+                                            onChange={(e) => setEmail(e.target.value)}
+                                            aria-required="true"
+                                             />
+                                    </Form.Group>
+                                    <Form.Group controlId="Birthday">
+                                        <Form.Label aria-label="birthday">Birthday:</Form.Label>
+                                        <Form.Control type="date"
+                                            value={birthday}
+                                            onChange={(e) => setBirthday(e.target.value)}
+                                            aria-required="true" 
+                                        />
+                                    </Form.Group>
+                                    <Button variant="primary" type="submit" aria-label="submit">Submit</Button> 
+                        </Form> 
                     </Card.Body>
                 </Card>
             </Col>
         </Row>
-</Container>
+    </Container>
 
     );
 };
