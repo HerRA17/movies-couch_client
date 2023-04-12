18fb995222112816d91856a46fe022c7967e3cc3
Hermann Rasch <hermann.rasch93@gmail.com>
2023-04-12 13:38:11 +0200

    Adjustments and addition to style & functions

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,43 +1,103 @@
-import React from "react"; 
+import React, { useState } from "react"; 
 import PropTypes from "prop-types";
 import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import axios from "axios";
+import { toast } from "react-bootstrap";
 
 // Movie card component
- function MovieCard ({ movie, removeFavMovie  }) {
+ function MovieCard ({ movie, user, updateUser  }) {
+  const [inFavoriteMovies, setInFavoriteMovies] = useState(user.FavoriteMovies.include(movies._id));
+  const token = window.localStorage.getItem("token");
+  // add Fav Movie function
+  const addFavoriteMovie = () => {
+    fetch(`https://movies-couch-api.vercel.app/users/${user.Username}/favMovies/${movie._id}`, {
+    method: "POST",
+    headers: {Authorization: `Bearer ${token}`}
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          toast.danger("Failed");
+          return false;
+        }
+      })
+      .then(user => {
+        if(user) {
+          toast.succes("Movie added to Favorite Movies");
+          setInFavoriteMovies(true);
+          updateUser(user);
+        }
+      })
+      .catch(e => {
+        toast.danger(e);
+      });
+  }    
+  const removeFavoriteMovie = () => {
+    fetch(`https://movies-couch-api.vercel.app/users/${user.Username}/favMovies/${movie._id}`, {
+      method: "POST",
+      headers: {Authorization: `Bearer ${token}`} 
+    })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        toast.danger("Failed");
+        return false;
+      }
+    })
+    .then(user => {
+      if(user) {
+        toast.succes("Movie deleted from Favorite Movies");
+        setInFavoriteMovies(false);
+        updateUser(user);
+      }
+    })
+    .catch(e => {
+      toast.danger(e);
+    });
+  }    
   
-
   return (
-      <Card className="movie-card">
+      <Card className="movie-card" style={{ width:"18rem"}}>
         <Card.Img variant="top" src={movie.ImageURL} alt="movie-poster"/> 
-        <Card.Body className="movie-card">
+        <Card.Body className="movie-card-body">
               <Card.Title>{movie.Title}</Card.Title>
-                <Card.Text>
+              <Card.Text>
                 <br />
-                {movie.Description}
+                {/* {movie.Description} */}
                 <br /><br />
                 {movie.Director.Name} 
                 <br /><br />
                 {movie.Genre.Name} 
-                </Card.Text>
+              </Card.Text>
                 <br />
                   <Link to={`/movies/${encodeURIComponent(movie._id)}`}>
                   <Button className="movie-card-button" active>Open</Button>
                   <br/> <br/>
-                  <Button 
-                  onClick={function (event) {event.preventDefault();
-                    removeFavMovie(movie._id);
-                  }} className="movie-card-button">Remove from Favorite List</Button> 
+                  {inFavoriteMovies ? <Button onClick={(e) => {
+                    preventDefault();
+                    removeFavoriteMovie(movie._id);
+                  }} 
+                  className="movie-card-button"
+                  >Remove from Favorite Movies</Button> :
+                  <Button onClick={(e) => {
+                    preventDefault();
+                    console.log(movie._id); 
+                    addFavoriteMovie(movie._id);
+                  }} 
+                  className="movie-card-button"
+                  >Add to Favorite Movies</Button>
+                  }
                   </Link>
             </Card.Body>
       </Card>
     );
     
 
-};
+}
 
-export {MovieCard};
+export { MovieCard };
 
 // defined props constrains for Movie Card
 MovieCard.propTypes = {
@@ -47,5 +107,7 @@ MovieCard.propTypes = {
     Description: PropTypes.string.isRequired,
     Director: PropTypes.object,
     Genre: PropTypes.object
-    }).isRequired
+  }).isRequired,
+  addFavoriteMovie: PropTypes.func,
+  removeFavoriteMovie: PropTypes.func
 };
