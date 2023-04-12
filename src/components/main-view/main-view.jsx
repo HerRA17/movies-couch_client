d006cf562af4be0818f52e253ae8a11f8af25971
Hermann Rasch <hermann.rasch93@gmail.com>
2023-04-12 13:38:46 +0200

    Adjustments and addition to style & functions

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -8,7 +8,7 @@ import { toast } from "react-bootstrap";
 import "react-toastify/dist/ReactToastify.css";
 
 import { MovieCard } from "../movie-card/movie-card";
-import { MovieView } from "../movie-view/movie-view"; 
+import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
 
@@ -30,18 +30,19 @@ function MainView()  {
     const onLoggedOut= function () {
         setUser(null);
         setToken(null);
-        localStorage.clear();}
+        localStorage.clear();
+      }
     useEffect(() => {
         //verifying token-authentication to access request
         if(!token) {
             return;
         }                
     fetch("https://movies-couch-api.vercel.app/movies", {
-    headers: {Authorization: `Bearer ${token}`},  
-    	})
-    .then((response) => response.json())
-    .then((movies) => {
-        console.log(movies); 
+      headers: {Authorization: `Bearer ${token}` },
+      })
+        .then((response) => response.json())
+        .then((movies) => {
+        // console.log(movies); 
         const moviesFromApi = movies.map((movie) => {
             // console.log(Object.keys(movies))
             return {
@@ -59,147 +60,101 @@ function MainView()  {
     
     });
 }, [token]);
-
-//add Fav-movie
-const addFavMovie = function (_id) {
-    axios.put(
-      `https://myflix-movie-app-elenauj.onrender.com/users/${user.Username}/topMovies/${movieId}`,
-      {
-       headers: {Authorization: `Bearer ${token}`} }
-    )
-      .then(function (response) {
-        if (response.status === 401) {
-          throw new Error(
-            "Sorry, you're not authorized to access this resource. "
-          );
-        } else if (response.status === 404) {
-            throw new Error('No such movie found.');
-        } else if (response.status === 409) {
-          throw new Error('You already added this movie to the list.');
-        } else if (response.ok) {
-          return response.json();
-        }
-      })
-      .then(function (updatedUser) {
-        toast.success('Movie has been added to your Favorite Movies.');
-        setUser(updatedUser);
-      })
-      .catch(function (error) {
-        if (error.message) {
-          toast.error(error.message);
-        } else {
-          toast.error(
-            'An error occurred while trying to add movie. Please try again later.'
-          );
-        }
-        console.error('An error occurred:' + error);
-      });
-  };
- // remove Fav-movie
-  const removeFavMovie = function (movieId) {
-    axios.delete(
-      `https://myflix-movie-app-elenauj.onrender.com/users/${user.Username}/topMovies/${movieId}`,
-      {
-        headers: {Authorization: `Bearer ${token}`},
-      }
-    )
-      .then(function (response) {
-        if (response.status === 401) {
-          throw new Error(
-            "Sorry, you're not authorized to access this resource. "
-          );
-        } else if (response.ok) {
-          return response.json();
-        }
-      })
-      .then(function (updatedUser) {
-        toast.success('Movie has been removed from your Favorite Movies.');
-        setUser(updatedUser);
-      })
-      .catch(function (error) {
-        if (error.message) {
-          toast.error(error.message);
-        } else {
-          toast.error(
-            'An error occurred while trying to delete. Please try again later.'
-          );
-        }
-        console.error('An error occurred:' + error);
-      });
-  }; 
-
+// update User function
+    const updateUser = (user) => {
+        fetch(`https://movies-couch-api.vercel.app/users/${user.Username}/`, {
+            headers: {Authorization: `Bearer ${token}` },
+        })
+        .then((response) => response.json())
+        .them((data) => {
+            if (data) {
+                console.log(data);
+                localStorage.setItem("user", JSON.stringify(data));
+                window.location.reload();
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    };
+/*
+Function where I add to the array-?
+favoriteMovies.add() or unshift()
+*/
+console.log(storedUser);
+console.log(user);
 
 
 return ( 
 <BrowserRouter>
-<ThemeProvider  breakpoints={["xxl","xl","lg","md","sm","xs"]}
-    minBreakpoint="xs">
+    <ThemeProvider  breakpoints={["xxl","xl","lg","md","sm","xs"]}
+        minBreakpoint="xs">
         <NavigationBar
-        user={user} 
-        onLoggedOut={onLoggedOut}
+            user={user} 
+            onLoggedOut={onLoggedOut}
          />
-    <Row  className="main-view" >
-        <Routes>
+        <Row  className="main-view" >
+            <Routes>
                 <Route
-                path="/signup"
-                element={
-                    <>
-                    {user ? (
-                    <Navigate to="/" />
-                    ):( <Col md={5}>
+                    path="/signup"
+                    element={
+                     <>
+                        {user ? (
+                        <Navigate to="/" />
+                        ) : ( <Col md={5}>
                             <SignupView />
                         </Col>
-                    )}
-                    </>
-                }
+                        )}
+                     </>
+                    }
                 />
                 <Route 
-                path="/login"
-                element={
-                    <>
-                    {user ? (
-                        <Navigate to="/" />
-                    ): ( <Col md={5}>
-                        <LoginView onLoggedIn={(user, token) => {setUser(user); setToken(token); }}  /> 
-                        </Col>
-                    )}
-                    </>
-                }
+                    path="/login"
+                    element={
+                     <>
+                        {user ? (
+                            <Navigate to="/" />
+                        ) : ( <Col md={5}>
+                            <LoginView onLoggedIn={(user, token) => {setUser(user); setToken(token); }}  /> 
+                            </Col>
+                        )}
+                     </>
+                    }
                 />
                 <Route
-                path="/movies/:movieId"
-                element={
-                    <>
-                    {!user ? (
+                    path="/movies/:movieId"
+                    element={
+                     <>
+                        {!user ? (
                         <Navigate to="/login" replace/>
-                        ): movies.length === 0 ? (
+                        ) : movies.length === 0 ? (
                             <div>The list is empty!</div>
                             ): ( <Col md={8}>
-                            <MovieView movies={movies} addFavMovie={addFavMovie} removeFavMovie={removeFavMovie}
+                            <MovieView movies={movies} /*addFavMovie={addFavMovie}*/ /*removeFavMovie={removeFavMovie}*/
                             favoriteMovies={user.favoriteMovies} />
                             </Col>
                         )}
-                    </>
-                }
+                     </>
+                    }
                 />
                 <Route
-                path="/"
-                element={
-                    <>
-                    {!user ? (
-                        <Navigate to="/login" replace/>
-                    ) : movies.length === 0 ? (
-                        <div>The list is empty!</div>
-                    ) : (
-                        <>
-                        {movies.map((movie) => (
-                        <Col className="mb-5" key={movie._id} md={4}>
-                            <MovieCard  movie={movie} />
-                        </Col>
-                     ))}
-                    </>
-                )}
-                </>
+                    path="/"
+                    element={
+                     <>
+                        {!user ? (
+                            <Navigate to="/login" replace/>
+                        ) : movies.length === 0 ? (
+                            <div>The list is empty!</div>
+                        ) : (
+                         <>
+                            {movies.map((movie) => (
+                            <Col className="mb-5" key={movie._id} md={4}>
+                                <MovieCard  movie={movie} user={user} updateUser={updateUser} />
+                            </Col>
+                            ))}
+                        </>
+                        )}
+                     </>
               }
             />
             <Route
@@ -209,22 +164,22 @@ return (
                     <>
                     {user ? (
                         <Col className="mb-5" >
-                            <TestProfile   user={user} setUser={setUser} movies={movies} onLoggedOut={onLoggedOut}/>
+                            <TestProfile   user={user} setUser={setUser} movies={movies} updateUser={updateUser} /*onLoggedOut={onLoggedOut}*/ />
                         {/* <ProfileView  token={token}  user={user}  setUser={setUser}  
-                        movies={movies} removeFavMovie={removeFavMovie} onLoggedOut={onLoggedOut}/>*/}
-                    </Col>
+                        movies={movies} removeFavMovie={removeFavMovie} onLoggedOut={onLoggedOut}/> */}
+                        </Col>
                     ) : (
-                    <Navigate to="/login" />
+                      <Navigate to="/login" />
                     )
-                }
-                </>
+                    }
+                    </>
               }
             />
-        </Routes>
-    </Row>
-</ThemeProvider>
+            </Routes>
+        </Row>
+    </ThemeProvider>
 </BrowserRouter>
     );
-};
+}
 
-export {MainView};
+export  { MainView };
