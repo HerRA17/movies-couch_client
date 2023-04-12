b55442c9b51b6692855207f12dcdfd77304b617c
Hermann Rasch <hermann.rasch93@gmail.com>
2023-04-12 13:39:30 +0200

    Minor adjustments

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,20 +1,19 @@
-import {React, useEffect} from "react";
+import { React, useEffect } from "react";
 import { Button, Card } from "react-bootstrap";
+import PropTypes from "prop-types";
 import { useParams } from "react-router";
 import { Link, Redirect } from "react-router-dom";
 import "./movie-view.scss";
 
-export const MovieView = ({ movies, favoriteMovies }) => {
-    const {movieId} = useParams();
+export const MovieView = ({ movies, favoriteMovies, addFavMovie, token }) => {
+    const { movieId } = useParams();
     const movie = movies.find((m) => m._id === movieId);
     
     if (!movie) {
         // if movie is not found return to homepage
         return <Redirect to="/" />;
-    };
-    // function addMovie(){
-    //     favoriteMovies.add();
-    //   }
+    }
+    
 return( 
     <Card className="movie-view">
         <Card.Img className="w-80" src={movie.ImageURL} alt="movie-poster"/>
@@ -34,9 +33,22 @@ return(
             <Link to={"/"}>
             <Button className="back-button" active>Back</Button>
             <br/> <br/>
-            <Button /*onClick={addMovie}*/>Add to Favorite List</Button>
             </Link>
         </Card.Body>
     </Card> 
     );
 };
+
+  
+MovieView.prototype = {
+    movies: PropTypes.shape({
+        _id: PropTypes.number,
+        Title: PropTypes.string,
+        ImageURL: PropTypes.string, 
+        Description: PropTypes.string, 
+        Director: PropTypes.string,
+        Genre: PropTypes.string
+    }),
+    favoriteMovies: PropTypes.array,
+    addFavMovie: PropTypes.func
+}
